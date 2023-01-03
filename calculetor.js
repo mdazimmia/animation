8ba0b852ff9e216cf8d4b6ed52bafca22d7f4f7d
mdazimmia <121815103+mdazimmia@users.noreply.github.com>
2023-01-03 13:37:46 +0600

    Add files via upload

---
+++ calculetor.js
@@ -0,0 +1,7 @@
+var input= document.querySelector("#input");
+for(i =0; i<30; i++){
+document.querySelectorAll("button")[i].addEventListener("click", function(){ 
+  //var text = this.innerHTML
+  //console.log(text);
+ 
+});
